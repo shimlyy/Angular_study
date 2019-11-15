e5dfccec32d18c6a9d886bc571173e352f393c72
shimlyy <xiaomengmiaomiao@gmail.com>
2019-11-15 15:19:31 +0900

    模块划分

---
+++ ECsite/src/app/my/my.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyRoutingModule } from './my-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MyRoutingModule
+  ]
+})
+export class MyModule { }
---
+++ ECsite/src/app/recommend/recommend.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RecommendRoutingModule } from './recommend-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RecommendRoutingModule
+  ]
+})
+export class RecommendModule { }
