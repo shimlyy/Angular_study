dae16171a01aaa2b6e97fa3967e4bc3dea6b33a9
shimlyy <xiaomengmiaomiao@gmail.com>
2019-11-17 19:32:03 +0900

    指令

--- ECsite/src/app/shared/directives/grid-item-image.directive.ts
+++ ECsite/src/app/shared/directives/grid-item-image.directive.ts
@@ -1,12 +1,15 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Input } from '@angular/core';
 
 @Directive({
   selector: '[appGridItemImage]',
 })
 export class GridItemImageDirective {
+  @Input() appGridItemImage = '2rem';
   constructor(private elr: ElementRef, private rd2: Renderer2) {
     this.rd2.setStyle(this.elr.nativeElement, 'grid-area', 'image');
-    this.rd2.setStyle(this.elr.nativeElement, '', `'image' 'title'`);
+    this.rd2.setStyle(this.elr.nativeElement, 'width', this.appGridItemImage);
+    this.rd2.setStyle(this.elr.nativeElement, 'height', this.appGridItemImage);
+    this.rd2.setStyle(this.elr.nativeElement, 'object-fit', 'cover');
    }
 
  }
