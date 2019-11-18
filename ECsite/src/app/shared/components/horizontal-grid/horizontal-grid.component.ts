20a4b11b17adec5f61d4489676429295cec60811
shimlyy <xiaomengmiaomiao@gmail.com>
2019-11-18 21:04:50 +0900

    组件嵌套和组件投影

--- ECsite/src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
+++ ECsite/src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 export interface Channel {
   id: number;
@@ -13,7 +13,28 @@ export interface Channel {
   styleUrls: ['./horizontal-grid.component.css']
 })
 export class HorizontalGridComponent implements OnInit {
+  @Input() cols = 8;
+  @Input() displayCols = 5;
+  sliderMargin = '0';
   constructor() {}
 
   ngOnInit() {}
+
+  public get scrollable() : boolean {
+    return this.cols > this.displayCols;
+  }
+
+  public get templateRows(): string {
+    return `minmax(auto, max-content)`;
+  }
+
+  public get templateColumns(): string {
+    return `repeat(${this.cols}, calc((100vw - ${this.displayCols *
+      0.4}rem) / ${this.displayCols}))`;
+  }
+
+  public handleScroll(ev) {
+    this.sliderMargin = `0 ${(100 * ev.target.scrollLeft) /
+      ev.target.scrollWidth}%`;
+  }
 }
