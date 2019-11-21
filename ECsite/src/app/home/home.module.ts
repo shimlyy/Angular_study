fe20e97a1f0acfe847f1a1b9f9477b3b49c83c36
shimlyy <xiaomengmiaomiao@gmail.com>
2019-11-21 21:22:58 +0900

    http 拦截器

--- ECsite/src/app/home/home.module.ts
+++ ECsite/src/app/home/home.module.ts
@@ -4,6 +4,8 @@ import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { HomeContainerComponent, HomeDetailComponent, HomeGrandComponent, ParentComponent, ChildComponent } from './components';
 import { HomeService } from './services';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ParamInterceptor, NotificationInterceptor } from './interceptors';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,18 @@ import { HomeService } from './services';
     ParentComponent,
     ChildComponent
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ParamInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: NotificationInterceptor,
+      multi: true
+    }
+  ],
   imports: [SharedModule, HomeRoutingModule]
 })
 export class HomeModule {}
